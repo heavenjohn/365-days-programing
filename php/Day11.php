edbbd51b0155a67c905585dd396bb40737356339
maxine duran <johnheaven48@gmail.com>
2026-02-15 23:43:53 +0800

    day11

---
+++ php/Day11.php
@@ -0,0 +1,9 @@
+<?php 
+// split string to an array of (single) characters
+
+$names = "heaven";
+$result = str_split($names);
+
+print_r($result);
+
+?>
---
+++ python/Day11.py
@@ -0,0 +1,6 @@
+# split string to an array of (single) characters
+
+names = "heaven"
+result = list(names)
+
+print(result)
